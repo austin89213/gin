cdfa971026ea0fdfaab5961a4c8963becd544afc
AustinLin <austinlin@flysheet.com.tw>
2022-02-08 13:52:23 +0800

    fix:gin-dump not working issues

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/tpkeeper/gin-dump v1.0.1 // indirect
 	github.com/ugorji/go v1.2.6 // indirect
 	golang.org/x/crypto v0.0.0-20220112180741-5e0467b6c7ce // indirect
 	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
--- server.go
+++ server.go
@@ -4,11 +4,12 @@ import (
 	"io"
 	"os"
 
-	"github.com/gin-gonic/gin"
-	// gindump "github.com/tpkeeper/gin-dump"
 	"golang-gin/controller"
 	"golang-gin/middlewares"
 	"golang-gin/service"
+
+	"github.com/gin-gonic/gin"
+	gindump "github.com/tpkeeper/gin-dump"
 )
 
 var (
@@ -25,8 +26,7 @@ func main() {
 	// server := gin.Default()
 	setupLogOutput()
 	server := gin.New()
-	server.Use(gin.Recovery(), middlewares.Logger(), middlewares.BasicAuth())
-	// server.Use(gin.Recovery(), middlewares.Logger(), middlewares.BasicAuth(), gindump.Dump())
+	server.Use(gin.Recovery(), middlewares.Logger(), middlewares.BasicAuth(), gindump.Dump())
 
 	server.GET("/test", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
